cbe8b99977b73063bd41ac516e1fd934d15f6fa2
Peter Hofmeier <phofmeier@googlemail.com>
2020-02-02 15:46:09 +0100

    time format

--- server/plot.py
+++ server/plot.py
@@ -4,7 +4,8 @@ import plotly.express as px
 import plotly.io as pio
 
 data = pd.read_csv("./test.log", sep=";")
-
+data['Time'] = pd.to_datetime(data['Time'], unit='ms', origin='unix', utc=True)
 fig = px.line(data, x="Time", y="Value")
+fig.update_layout(xaxis = dict(tickformat = "%Y-%m-%d %H:%M:%S.%L"))
 
 pio.write_html(fig, file="plot.html", auto_open=False)
