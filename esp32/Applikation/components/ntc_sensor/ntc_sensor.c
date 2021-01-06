ec8770455e83c2c4c8738a6f319e7debf61fe5ce
Peter Hofmeier <phofmeier@googlemail.com>
2021-01-06 14:47:53 +0100

    better adc reading

--- esp32/Applikation/components/ntc_sensor/ntc_sensor.c
+++ esp32/Applikation/components/ntc_sensor/ntc_sensor.c
@@ -13,21 +13,25 @@
 #include "freertos/queue.h"
 #include "driver/gpio.h"
 #include "driver/adc.h"
+#include "esp_adc_cal.h"
 #include "esp_system.h"
 #include "../send_to_server/send_to_server.c"
 #include "../gui/gui.c"
 
 #define SENSOR_ADC_CHANNEL_1 6
 #define SENSOR_ADC_CHANNEL_2 7
+#define DEFAULT_VREF    1100
 
 static const adc_bits_width_t width = ADC_WIDTH_BIT_12;
+static const adc_atten_t atten = ADC_ATTEN_DB_11;
+static esp_adc_cal_characteristics_t *adc_chars;
 static const char *NTC_TAG = "NTC";
 // Model Parameter
 static const double R_N = 100e3;
 static const double T_N = 298.15;
 static const double U_ges = 3.3;
 static const double B = 3.741987e3;
-static const double R_1 = 22e3;
+static const double R_1 = 24.9e3;
 
 typedef struct
 {
@@ -35,9 +39,9 @@ typedef struct
     QueueHandle_t wifiSendEvent;
 } GuiWifiQueues_t;
 
-double tempFromADC(double adc_value)
+double tempFromMilliVolt(double voltage_m)
 {
-    double U_meas = U_ges / 4096 * adc_value;
+    double U_meas = voltage_m * 1e-3;
     double R_NTC = U_meas * R_1 / (U_ges - U_meas);
     double T_kelvin = 1 / (log(R_NTC / R_N) / B + 1 / T_N);
     return T_kelvin - 273.15;
@@ -60,9 +64,13 @@ void task_ntc_sensor(void *EventQueue)
     assert(r == ESP_OK);
     
     // init
-    adc1_config_channel_atten(SENSOR_ADC_CHANNEL_1, ADC_ATTEN_11db);
-    adc1_config_channel_atten(SENSOR_ADC_CHANNEL_2, ADC_ATTEN_11db);
+    adc1_config_channel_atten(SENSOR_ADC_CHANNEL_1, atten);
+    adc1_config_channel_atten(SENSOR_ADC_CHANNEL_2, atten);
     adc1_config_width(width);
+
+    // calibration
+    adc_chars = calloc(1, sizeof(esp_adc_cal_characteristics_t));
+    esp_adc_cal_value_t val_type = esp_adc_cal_characterize(ADC_UNIT_1, atten, width, DEFAULT_VREF, adc_chars);
     
     vTaskDelay(10000 / portTICK_PERIOD_MS);
 
@@ -84,13 +92,15 @@ void task_ntc_sensor(void *EventQueue)
     for (int i = 0;; i++)
     {   
         uint32_t reading =  adc1_get_raw(SENSOR_ADC_CHANNEL_1);
+        reading = esp_adc_cal_raw_to_voltage(reading, adc_chars);
         sensor_value_mean_1 = sensor_value_mean_1 + 0.1 * ((double)reading - sensor_value_mean_1);
         reading =  adc1_get_raw(SENSOR_ADC_CHANNEL_2);
+        reading = esp_adc_cal_raw_to_voltage(reading, adc_chars);
         sensor_value_mean_2 = sensor_value_mean_2 + 0.1 * ((double)reading - sensor_value_mean_2);
         
         if ((i % 50) == 0){
-            gui_event_s1.lDataValue = tempFromADC(sensor_value_mean_1);
-            gui_event_s2.lDataValue = tempFromADC(sensor_value_mean_2);
+            gui_event_s1.lDataValue = tempFromMilliVolt(sensor_value_mean_1);
+            gui_event_s2.lDataValue = tempFromMilliVolt(sensor_value_mean_2);
             xQueueSendToBack(xGuiEventQueue, &gui_event_s1, 0);
             xQueueSendToBack(xGuiEventQueue, &gui_event_s2, 0);
         }
